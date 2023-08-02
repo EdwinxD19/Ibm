d87260d6656cc64caf76ab4212d3b2b43a9f99a2
EdwinxD19 <66256532+EdwinxD19@users.noreply.github.com>
2023-08-01 21:50:23 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# ### Some of the popular languages that Data Scientists use are:
+
+# * C
+# * Python
+# * Javascript
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 
+# - Panda
+# - Numpy 
+# - gplot
+
+# 
+# |Data Science Tools|
+# |------------------ |
+#  |R|
+# |Git|
+# |Apache|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# * print("2+2=4")
+# * print("2-2=0")
+# * print("2/2=1")
+# * print("2x2=4")
+
+# In[4]:
+
+
+R = (3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+print(R)
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+hours = 200 / 60
+print(hours)
+
+
+# **Objectives:**
+# * Python
+# * C
+# * Java
+# * SQL
+# 
+
+# ## Author
+# Edwin Bolon
